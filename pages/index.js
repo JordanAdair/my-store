8543806f57fb72260c33f12ac03be98d908cf628
Jordan <43575081+JordanAdair@users.noreply.github.com>
2022-04-22 21:36:15 -0400

    Connecting "Sanity" backend with our Frontend.

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,40 @@
 import React from "react";
 
+import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
+const Home = ({ productData, bannerData }) => {
 	return (
 		<>
-			<HeroBanner />
+			{/* If the bannerData prop successfully returned an array, return the first value of the array */}
+			<HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
 			<div className="products-heading">
 				<h2>Best Selling Products</h2>
 				<p>Speakers</p>
 			</div>
 			<div className="products-container">
-				{["Product 1", "Product 2"].map((product) => product)}
+				{/* If the productData prop successfully returned an array, return each product name */}
+				{productData?.map((product) => product.name)}
 			</div>
 
 			<FooterBanner />
 		</>
 	);
+};
+
+export const getServerSideProps = async () => {
+	//Grab all our products from our Sanity dashboard
+	const productQuery = '*[_type == "product"]';
+	const productData = await client.fetch(productQuery);
+
+	//Grab the banner data from our Sanity dashboard
+	const bannerQuery = '*[_type == "banner"]';
+	const bannerData = await client.fetch(bannerQuery);
+
+	return {
+		props: { productData, bannerData },
+	};
 };
 
 export default Home;
