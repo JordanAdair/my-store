00219ac0f3528159de8b7c43a2417e8cd2ab8488
Jordan <43575081+JordanAdair@users.noreply.github.com>
2022-04-23 16:16:52 -0400

    Cleaned up the layout, and added my socials to the footer.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className="footer-container">
+			<p>2022 MyStore™ | Jordan Adair</p>
+			<p className="icons">
+				<a href="https://www.linkedin.com/in/jordan-adair-2708b7178/">
+					<AiFillLinkedin />
+				</a>
+				<a href="https://github.com/JordanAdair">
+					<AiFillGithub />
+				</a>
+			</p>
+		</div>
+	);
 };
 
 export default Footer;
